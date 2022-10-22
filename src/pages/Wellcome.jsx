169dd14e46091331eca9d67cde0302578366a390
Alan Alonso <alan.alonso.gaf15@gmail.com>
2022-10-22 18:19:27 -0300

    landingpage updated

--- src/pages/Wellcome.jsx
+++ src/pages/Wellcome.jsx
@@ -26,7 +26,7 @@ const WellcomeContainer = styled.div`
     gap: 0.5rem;
 
     a {
-      padding: 0.75rem;
+      padding: 0.5rem;
       border: none;
       border-radius: 0.25rem;
       text-align: center;
@@ -46,12 +46,10 @@ const WellcomeContainer = styled.div`
   }
 
   .figure-logo {
-
     article {
       display: flex;
       width: 100%;
-      gap:.25rem;
-
+      gap: 0.25rem;
 
       div {
         width: 20px;
@@ -59,53 +57,97 @@ const WellcomeContainer = styled.div`
         background-color: red;
       }
 
-      div:nth-child(1){
-      background-color:#F5A38A;
+      div:nth-child(1) {
+        background-color: #f5a38a;
       }
-      div:nth-child(2){
-      background-color:#f3C57D;
+      div:nth-child(2) {
+        background-color: #f3c57d;
       }
-      div:nth-child(3){
-      background-color:#DDE595;
+      div:nth-child(3) {
+        background-color: #dde595;
       }
-      div:nth-child(4){
-      background-color:#7BD5E1;
+      div:nth-child(4) {
+        background-color: #7bd5e1;
       }
-      div:nth-child(5){
-      background-color:#C78DD0;
+      div:nth-child(5) {
+        background-color: #c78dd0;
       }
     }
   }
 
   .main-content {
     display: flex;
+    justify-content:center;
     flex-direction: column;
     gap: 2rem;
+    min-height:100vh;
   }
 
-  @media (min-width:800px){
-  justify-content:center;
-  align-items:center;
-  text-align:center;
+  .img{
+width: 96%;
+max-width: 960px;
+margin: 2rem auto;
+}
 
-  .figure-logo{
-  position:absolute;
-  top:1rem;
-  left:1rem;
+  img{
+width: 100%;
+height: auto; 
+}
+
+  @media (min-width: 800px) {
+    justify-content: start;
+    align-items: center;
+    text-align: center;
+
+    .body{
+    width:1000px;
+    display:flex;
+    flex-direction:column;
+    gap:5rem;
+    }
+
+    .side{
+    display:flex;
+   text-align:left;
+   gap:2rem;
+   align-items:center;
+
+   p{
+   width:500px;
+   }
+    }
+
+   .side:nth-child(2) {
+   flex-direction:row-reverse;
+   text-align:right;
+   }
+
+    .main-content {
+      justify-content: center;
+      align-items: center;
+      gap: 3rem;
+      min-height:100vh;
+
+      a {
+        width: 200px;
+      }
+
+      a:hover {
+        transform: scale(1.025);
+      }
+
+    }
   }
 
-  .main-content{
-  justify-content:center;
-  align-items:center;
-  gap:3rem;
+  footer{
+  padding:1rem;
+  margin-top:4rem;
 
   a{
-  width:200px;
-  }
-
-  a:hover{
-  transform:scale(1.1);
-  }
+  color:inherit;
+  font-weight:bold;
+  text-decoration:none;
+  display:inline-block;
   }
   }
 `;
@@ -114,25 +156,37 @@ const Wellcome = () => {
   return (
     <WellcomeContainer>
       <Loader start=".5s" />
-      <div className="figure-logo">
-        <article>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </article>
-      </div>
       <div className="main-content">
         <div>
           <h1>Min note</h1>
-          <p>A minimal note app where you can save any idea quick and easy</p>
+          <p>Um aplicativo de notas mínimo onde você pode salvar qualquer ideia de forma rápida e fácil</p>
         </div>
         <div className="buttons-container">
           <Link to="/signin">Sig in</Link>
           <Link to="/signup">Sig up</Link>
         </div>
       </div>
+      <div className="body">
+        <article className="side">
+          <p>Min notes é um aplicativo web simples e fácil de usar. focado em um usuário minimalista fornecendo a função de salvar notas, ideias, receitas ou o que você precisar em notas básicas armazenadas na nuvem. dando-lhe a opção de visualizar, editar ou excluir essas notas de forma rápida e fácil</p>
+          <div className="img">
+            <img src='http://imgfz.com/i/bCtYDjB.png'/>
+          </div>
+        </article>
+        <article className="side">
+          <p>Além de dar a opção de mudar a cor dos cartões ou notas, podendo diferenciá-los facilmente. 5 cores, 5 tipos ou temas diferentes se desejar</p>
+          <div className="img">
+            <img src='http://imgfz.com/i/M2RDcIu.png'/>
+          </div>
+        </article>
+        <article className="side">
+          <p>Ao ter várias notas armazenadas, é fácil demorar para encontrar uma específica ou perder diretamente uma nota. Mas Min notes oferece um filtro de pesquisa fácil de usar com o qual você pode pesquisar títulos ou palavras escritas no corpo das notas para encontrar a nota que procura mais rapidamente.</p>
+          <div className="img">
+            <img src='http://imgfz.com/i/Ra3OBtz.png'/>
+          </div>
+        </article>
+      </div>
+      <footer>make by Alan G. Alonso ( <a href='https://github.com/touzand' target='_blank'>Touzand</a> )</footer>
     </WellcomeContainer>
   );
 };
