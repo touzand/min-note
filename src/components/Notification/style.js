9fe4ffe945c2cb89beef35c33023ced356dbdccb
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-02-12 19:19:19 -0300

    cleaned...

--- src/components/Notification/style.js
+++ src/components/Notification/style.js
@@ -1,43 +1,32 @@
-import styled,{keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const PopUp = keyframes`
   0%{bottom:5rem;opacity:0;}
   100%{bottom:1rem;opacity:1;}
-`
-
-export const NotificationContainer = styled.div`
-position:absolute;
-width:300px;
-//top:50%;
-//left:50%;
-//top:5rem;
-  //transform:translate(-50%,-50%);
-  right:1rem;
-  bottom:5rem;
-  opacity:0;
-height:auto;
-//background-color:#dc3545;
-font-weight:bold;
-text-align:center;
-//outline:solid thin whitesmoke;
-//outline-offset:-3px;
-animation:${PopUp} .2s ease-in-out both;
-//border-radius:.5rem;
-  //margin-top:3rem;
-  //padding:1rem
-  
-  p{
-    margin:0;
-    padding:1rem;
-    border-radius:.25rem;
-  }
-
-  & .error{
-    background-color:red;
-  }
-
-  & .success{
-    background-color:green;
-  }
 `;
 
+export const NotificationContainer = styled.div`
+  position: fixed;
+  width: 300px;
+  right: 1rem;
+  bottom: 5rem;
+  opacity: 0;
+  height: auto;
+  font-weight: bold;
+  text-align: center;
+  animation: ${PopUp} 0.2s ease-in-out both;
+
+  p {
+    margin: 0;
+    padding: 1rem;
+    border-radius: 0.25rem;
+  }
+
+  & .error {
+    background-color: red;
+  }
+
+  & .success {
+    background-color: green;
+  }
+`;
