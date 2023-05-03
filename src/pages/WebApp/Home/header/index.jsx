1f2a7b8144b6ba7cafd2bcc0b796df9a3685a10d
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-05-03 12:18:34 -0300

    Update margin and add MenuGrid icon to Header component.

--- src/pages/WebApp/Home/header/index.jsx
+++ src/pages/WebApp/Home/header/index.jsx
@@ -8,6 +8,7 @@ import { Hr, IconButtom } from "../../../../styled-components";
 import { IoMdAdd } from "react-icons/io";
 import { BsSearch } from "react-icons/bs";
 import { GoSignOut } from "react-icons/go";
+import { CgMenuGridO } from 'react-icons/cg'
 import HeaderTop from "../../../../components/Header";
 
 const Header = (props) => {
@@ -50,39 +51,46 @@ const Header = (props) => {
         >
           <h1>All notes</h1>
           <div className="header-subsection">
-            {!isMobile && (
               <IconButtom
                 tc={`#F6F1E9`}
-                left={true}
                 onClick={handdleAdd}
                 translate="no"
               >
-                <IoMdAdd/>
+                <CgMenuGridO/>
               </IconButtom>
-            )}
-
-            <IconButtom
-              className="search"
-              onClick={() => props.setSearchVisible(true)}
-              translate="no"
-              tc={`#F6F1E9`}
-            >
-              <BsSearch/>
-            </IconButtom>
-            <IconButtom
-              tc={`#F6F1E9`}
-              onClick={() => setOptionMessage(true)}
-              translate="no"
-              right={!isMobile && true}
-            >
-              <GoSignOut/>
-            </IconButtom>
           </div>
         </article>
       </HeaderTop>
-        <Hr tc={`var(--font-color)`} className={`${props.query ? "up-header" : "down-header"}`} />
     </>
   );
 };
 
 export default Header;
+
+            //{!isMobile && (
+              //<IconButtom
+                //tc={`#F6F1E9`}
+                //left={true}
+                //onClick={handdleAdd}
+                //translate="no"
+              //>
+                //<IoMdAdd/>
+              //</IconButtom>
+            //)}
+
+            //<IconButtom
+              //className="search"
+              //onClick={() => props.setSearchVisible(true)}
+              //translate="no"
+              //tc={`#F6F1E9`}
+            //>
+              //<BsSearch/>
+            //</IconButtom>
+            //<IconButtom
+              //tc={`#F6F1E9`}
+              //onClick={() => setOptionMessage(true)}
+              //translate="no"
+              //right={!isMobile && true}
+            //>
+              //<GoSignOut/>
+            //</IconButtom>
