b5d318793f65ccdc7ee3a2f074a141c2d867ee81
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-05-04 12:11:28 -0300

    Home/index.jsx updated

--- src/pages/WebApp/Home/index.jsx
+++ src/pages/WebApp/Home/index.jsx
@@ -1,62 +1,58 @@
-import { userAuth } from "../../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Loader from "../../../components/loader";
-import Note from "../../../components/Card/Note.jsx";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../../firebase.config.js";
-import { NotesGeneralContainer } from "./style";
-import Header from "./header";
-import InputSearch from "./inputSearch";
-import NoNotes from "./noNotes";
-import useIsMobile from "../../../hooks/useIsMobile";
-import Filter from "./filter";
+import {userAuth} from '../../../contexts/AuthContext';
+import {useNavigate} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import Loader from '../../../components/loader';
+import Note from '../../../components/Card/Note.jsx';
+import {collection, getDocs} from 'firebase/firestore';
+import {db} from '../../../firebase.config.js';
+import {NotesGeneralContainer} from './style';
+import Header from './header';
+import InputSearch from './inputSearch';
+import NoNotes from './noNotes';
+import useIsMobile from '../../../hooks/useIsMobile';
+import Filter from './filter';
 import Menu from './menu';
 
-const Home = ({ children }) => {
+const Home = ({children}) => {
   const [searchVisible, setSearchVisible] = useState(false);
   const [menu, setMenu] = useState(true);
   const [data, setData] = useState([]);
-  const [query, setQuery] = useState("");
-  const { user } = userAuth();
+  const [query, setQuery] = useState('');
+  const {user} = userAuth();
   const navigate = useNavigate();
   const isMobile = useIsMobile(800);
 
   useEffect(() => {
     if (user === null) {
-      navigate("/signin");
+      navigate('/signin');
     }
   }, [user]);
 
   useEffect(() => {
     (async () => {
       const querySnapshot = await getDocs(collection(db, `${user.uid}`));
-      setData(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setData(querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id})));
     })();
   }, [user]);
 
   const dataFilter =
-    data.filter((nota) =>
-      nota.title.toLowerCase().includes(query.toLowerCase())
+    data.filter(nota =>
+      nota.title.toLowerCase().includes(query.toLowerCase()),
     ) &&
-    data.filter((nota) =>
-      nota.body.toLowerCase().includes(query.toLowerCase())
-    );
+    data.filter(nota => nota.body.toLowerCase().includes(query.toLowerCase()));
 
   const handdleAdd = () => {
     document
-      .querySelector(".add-background-transition")
-      .classList.add("background-transition-expand");
+      .querySelector('.add-background-transition')
+      .classList.add('background-transition-expand');
     setTimeout(() => {
-      navigate("/new");
+      navigate('/new');
     }, 600);
   };
 
   return (
     <>
-      {menu && (
-        <Menu setMenu={setMenu}/>
-      )}
+      {menu && <Menu setMenu={setMenu} />}
       <NotesGeneralContainer>
         <div className="add-background-transition"></div>
         <Loader start="1s" />
@@ -71,16 +67,15 @@ const Home = ({ children }) => {
           query={query}
           searchVisible={searchVisible}
           setSearchVisible={setSearchVisible}
- setMenu={setMenu}
+          setMenu={setMenu}
         />
-        <Filter query={query} setQuery={setQuery}/>
+        <Filter query={query} setQuery={setQuery} />
         <div
-          className={` masonry-grid ${
-            query ? "up-header" : "down-header"
-          } ${data.length === 0 ? "no-notes-cont" : "with-notes-cont"}`}
-        >
+          className={` masonry-grid ${query ? 'up-header' : 'down-header'} ${
+            data.length === 0 ? 'no-notes-cont' : 'with-notes-cont'
+          }`}>
           {data.length !== 0 ? (
-            dataFilter.map((note) => (
+            dataFilter.map(note => (
               <Note
                 bg={note.bg}
                 title={note.title}
@@ -97,8 +92,7 @@ const Home = ({ children }) => {
           <button
             onClick={handdleAdd}
             className="icon-button add"
-            translate="no"
-          >
+            translate="no">
             <span className="material-symbols-outlined">add</span>
           </button>
         )}
