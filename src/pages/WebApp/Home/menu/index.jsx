3ad3ad06e9f1acdb40abb60181deebae3c591f39
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-05-06 09:05:39 -0300

    Add logic to count background occurrences in Menu component.

--- src/pages/WebApp/Home/menu/index.jsx
+++ src/pages/WebApp/Home/menu/index.jsx
@@ -1,13 +1,28 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {BackgroundContainer, ContentContainer, FigureColor} from './style';
 
 const Menu = props => {
-  useEffect(() => {}, []);
+  const [bgCounts, setBgCounts] = useState({});
+
+  useEffect(() => {
+    if (props.data.length != 0) {
+      setBgCounts(() => countBgOccurrences(props.data));
+    }
+  }, [props.data]);
+
+  const countBgOccurrences = arr => {
+    return arr.reduce((countMap, obj) => {
+      const {bg} = obj;
+      countMap[bg] = (countMap[bg] || 0) + 1;
+      return countMap;
+    }, {});
+  };
 
   return (
     <BackgroundContainer onClick={() => props.setMenu(false)} menu={props.menu}>
       <ContentContainer>
         <article>
+          {console.log(bgCounts['#DDDDDD'])}
           <div className="row">
             <h3>All notes</h3>
             <h3>{props.data.length}</h3>
@@ -19,42 +34,42 @@ const Menu = props => {
               <FigureColor bg="#F5A38A" />
               <h3>Nombre del color</h3>
             </div>
-            <h3>{props.countData.red}</h3>
+            <h3>{bgCounts['#F5A38A'] ?? 0}</h3>
           </button>
           <button>
             <div>
-              <FigureColor bg="#f3C57D" />
+              <FigureColor bg="#F3C57D" />
               <h3>Nombre del color</h3>
             </div>
-            <h3>{props.countData.orange}</h3>
+            <h3>{bgCounts['#F3C57D'] ?? 0}</h3>
           </button>
           <button>
             <div>
-              <FigureColor bg="#dde595" />
+              <FigureColor bg="#DDE595" />
               <h3>Nombre del color</h3>
             </div>
-            <h3>{props.countData.green}</h3>
+            <h3>{bgCounts['#DDE595'] ?? 0}</h3>
           </button>
           <button>
             <div>
-              <FigureColor bg="#7bd5e1" />
+              <FigureColor bg="#7BD5E1" />
               <h3>Nombre del color</h3>
             </div>
-            <h3>{props.countData.blue}</h3>
+            <h3>{bgCounts['#7BD5E1'] ?? 0}</h3>
           </button>
           <button>
             <div>
-              <FigureColor bg="#c78dd0" />
+              <FigureColor bg="#C78DD0" />
               <h3>Nombre del color</h3>
             </div>
-            <h3>{props.countData.purple}</h3>
+            <h3>{bgCounts['#C78DD0'] ?? 0}</h3>
           </button>
           <button>
             <div>
-              <FigureColor bg="#dddddd" />
+              <FigureColor bg="#DDDDDD" />
               <h3>Nombre del color</h3>
             </div>
-            <h3>{props.countData.white}</h3>
+            <h3>{bgCounts['#DDDDDD'] ?? 0}</h3>
           </button>
         </article>
 
