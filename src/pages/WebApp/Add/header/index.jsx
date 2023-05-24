58b52e8f21e740936d98dac27764505add021c86
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-05-24 12:36:28 -0300

    refactor(add): header props

--- src/pages/WebApp/Add/header/index.jsx
+++ src/pages/WebApp/Add/header/index.jsx
@@ -3,7 +3,7 @@ import {Hr, IconButtom} from '../../../../styled-components';
 import {HeaderContainer} from './style';
 import {BiArrowBack, BiFontFamily} from 'react-icons/bi';
 import {VscSettingsGear} from 'react-icons/vsc';
-import {HiOutlineColorSwatch, HiOutlineSave} from 'react-icons/hi';
+import {HiOutlineColorSwatextColorContrasth, HiOutlineSave} from 'react-icons/hi';
 import HeaderTop from '../../../../components/Header';
 import {FaAlignJustify, FaAlignLeft, FaAlignRight} from 'react-icons/fa';
 import {useState} from 'react';
@@ -13,26 +13,24 @@ const Header = props => {
   const [textAlignVisible, setTextAlignVisible] = useState(false);
   const [colorPicker, setColorPicker] = useState(false);
   const {
-    textContrast,
     setVisible,
     handdleAddDoc,
-    color,
-    setTextAlign,
     setConfigPanelOpen,
     configPanelOpen,
+    noteContent
   } = props;
 
   return (
     <>
-      <HeaderTop color={color}>
-        <IconButtom both={true} tc={`${textContrast}`}>
+      <HeaderTop color={noteContent.background_color}>
+        <IconButtom both={true} textColorContrast={`${noteContent.text_color_contrast}`}>
           <Link to="/" translate="no">
             <BiArrowBack />
           </Link>
         </IconButtom>
         <div className="header-subsection">
           <IconButtom
-            tc={`${textContrast}`}
+            textColorContrast={`${noteContent.text_color_contrast}`}
             left={true}
             translate="no"
             onClick={() => setConfigPanelOpen(!configPanelOpen)}
@@ -40,7 +38,7 @@ const Header = props => {
             <VscSettingsGear />
           </IconButtom>
           <IconButtom
-            tc={`${textContrast}`}
+            textColorContrast={`${noteContent.text_color_contrast}`}
             right={true}
             onClick={handdleAddDoc}
             translate="no">
@@ -51,8 +49,8 @@ const Header = props => {
       <Hr
         fadeIn={true}
         opac={true}
-        tc={`${textContrast}`}
-        style={{borderColor: `${textContrast}70`}}
+        textColorContrast={`${noteContent.text_color_contrast}`}
+        style={{borderColor: `${noteContent.text_color_contrast}70`}}
       />
     </>
   );
