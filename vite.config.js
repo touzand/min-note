d4433ddb6b0c28e087f0e381924db2c4fa473eca
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-06-29 18:07:07 -0300

    feat(vite.config): setver.open / open browser when starting sever

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/min-note/app/',
+  server: {
+    open: '/min-note/app/',
+  },
   plugins: [
     react({
       include: '**/*.jsx',
