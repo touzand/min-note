343d0da0a4acb74222e8fd30ed2bd7621071fbc9
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-11-30 20:44:58 -0300

    fix(home): noNotes component updated

--- src/pages/WebApp/Home/noNotes/index.jsx
+++ src/pages/WebApp/Home/noNotes/index.jsx
@@ -3,8 +3,8 @@ import {NoNotesContainer} from "./style";
 const NoNotes = () => {
   return (
     <NoNotesContainer>
-      <h3>You dont have any notes :( or maybe it is still loading...</h3>
-      <h3>Click the " + " button to create a new note</h3>
+      <h3>You dont have any notes :( or maybe it is still loading</h3>
+      <h3>So click the " + " button to create a new note</h3>
     </NoNotesContainer>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/min-note/app/',
+  base: '/min-note',
   server: {
-    open: '/min-note/app/',
+    open: '/min-note',
   },
   plugins: [
     react({
