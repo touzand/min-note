3763c591504bb927644c19c2c6bb0f27d1a24895
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-11-30 20:38:29 -0300

    fix(home): noNotes component updated

--- src/pages/WebApp/Home/noNotes/index.jsx
+++ src/pages/WebApp/Home/noNotes/index.jsx
@@ -3,7 +3,7 @@ import {NoNotesContainer} from "./style";
 const NoNotes = () => {
   return (
     <NoNotesContainer>
-      <h3>You dont have any notes :( or it is still loading</h3>
+      <h3>You dont have any notes :( or maybe it is still loading...</h3>
       <h3>Click the " + " button to create a new note</h3>
     </NoNotesContainer>
   );
