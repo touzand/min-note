148c9b0ff645342432d5f3e1bc5da6dd677890dc
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-11-30 21:00:16 -0300

    package.json updated

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/min-note',
+  base: '/min-notes',
   server: {
-    open: '/min-note',
+    open: '/min-notes',
   },
   plugins: [
     react({
