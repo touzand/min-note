87e0da8df177d3b0e0365f00922fc6596ba52924
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-12-03 21:03:27 -0300

    feat(master): something for deploy

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY  || 'mock_key',
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
