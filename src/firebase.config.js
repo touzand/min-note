85ad3d0fd0f35f161e3e68c53c99ece792c872f2
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-12-16 19:07:18 -0300

    update(firestore): master update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 
@@ -8,10 +9,12 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGIN_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+const analytics = getAnalytics(app);
