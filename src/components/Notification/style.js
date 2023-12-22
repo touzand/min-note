9502ea4a4e1f48bc79b8bffe56de4754402a8cb0
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-12-21 21:32:46 -0300

    update(notification): animation updated

--- src/components/Notification/style.js
+++ src/components/Notification/style.js
@@ -1,20 +1,20 @@
 import styled, { keyframes } from "styled-components";
 
 const PopUp = keyframes`
-  0%{bottom:5rem;opacity:0;}
-  100%{bottom:1rem;opacity:1;}
+  0%{right:-5rem;opacity:1;}
+  100%{right:1rem;opacity:1;}
 `;
 
 export const NotificationContainer = styled.div`
   position: fixed;
   width: 300px;
-  right: 1rem;
-  bottom: 5rem;
+  bottom: 1rem;
+  right: -5rem;
   opacity: 0;
   height: auto;
   font-weight: bold;
   text-align: center;
-  animation: ${PopUp} 0.2s ease-in-out both;
+  animation: ${PopUp} 0.5s ease-in-out both;
 
   p {
     margin: 0;
